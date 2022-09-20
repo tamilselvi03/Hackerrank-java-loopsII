e7d46d6ecc8b645b4490eee78ff7d9f00e944fa9
Tamilselvi <112626100+tamilselvi03@users.noreply.github.com>
2022-09-20 09:42:07 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SquareRoot
+{
+	public static void main(String[] args) {
+	           Scanner sc=new Scanner(System.in);
+	           int a=sc.nextInt();
+	           double x=Math.sqrt(a);
+	           System.out.print(x);
+	}
+}
